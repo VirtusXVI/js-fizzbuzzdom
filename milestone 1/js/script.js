9417bf18bd1830e6fc1b4ca7df46b5a5f78a720c
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-07 14:40:16 +0200

    completed milestone 1

--- milestone 1/js/script.js
+++ milestone 1/js/script.js
@@ -1,9 +1,16 @@
 let userContainer = document.querySelector(".container");
+let userOutput = "";
 
-
-for(let i = 1; i <= 1000 ; i++){
+for(let i = 1; i <= 100 ; i++){
     
-    let newBox = `<div class="box">${i}</div>`;
-
-    userContainer.innerHTML += newBox;
+    if(i % 3 === 0 && i % 5 === 0){
+        userOutput = "FizzBuzz";
+    }else if(i % 5 === 0){
+        userOutput = "Buzz";
+    }else if(i % 3 === 0){
+        userOutput = "Fizz";
+    }else{
+        userOutput = i;   
+    };
+    console.log(userOutput);
 };
