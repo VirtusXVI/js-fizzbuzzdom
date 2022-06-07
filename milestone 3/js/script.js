b0b4cbb2df00a2a25bc2f0dd4ca297892a6e828a
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-07 15:14:02 +0200

    completed milestone 3

--- milestone 3/js/script.js
+++ milestone 3/js/script.js
@@ -1,9 +1,16 @@
 let userContainer = document.querySelector(".container");
 
-
-for(let i = 1; i <= 1000 ; i++){
-    
-    let newBox = `<div class="box">${i}</div>`;
-
-    userContainer.innerHTML += newBox;
+for(let i = 1; i <= 100 ; i++){
+    // Controllo se il numero è divisibile per 3, 5 o entrambi
+    if(i % 3 === 0 && i % 5 === 0){
+        userOutput = `<div class="box fizzbuzz">FizzBuzz</div>`;
+    }else if(i % 5 === 0){
+        userOutput = `<div class="box buzz">Buzz</div>`;
+    }else if(i % 3 === 0){
+        userOutput = `<div class="box fizz">Fizz</div>`;
+    }else{
+        userOutput = `<div class="box">${i}</div>`; 
+    };
+    // Inserisco nella pagina la box con il numero o l'eventuale parola
+    userContainer.innerHTML += userOutput;
 };
