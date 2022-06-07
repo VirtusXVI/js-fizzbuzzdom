d587547800d75ad7f7ef47d14cc86e72cabd6211
VirtusXVI <manuel.aveta2001@gmail.com>
2022-06-07 14:59:40 +0200

    completed milestone 1 and 2 with comments

--- milestone 1/js/script.js
+++ milestone 1/js/script.js
@@ -2,6 +2,7 @@ let userOutput = "";
 
 
 for(let i = 1; i <= 100 ; i++){
+    // Controllo se il numero è divisibile per 3, 5, o entrambi
     if(i % 3 === 0 && i % 5 === 0){
         userOutput = "FizzBuzz";
     }else if(i % 5 === 0){
@@ -11,5 +12,6 @@ for(let i = 1; i <= 100 ; i++){
     }else{
         userOutput = i;   
     };
+    // Stampo nella console il risultato
     console.log(userOutput);
 }; 
